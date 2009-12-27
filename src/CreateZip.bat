39f650745777c61fb9438b64fa5057ecfc78e670
hmmueller <harald_m_mueller@gmx.de>
2009-12-27 19:08:19 +0100

    [svn r3] Added bat-file for .zip creation

---
+++ src/CreateZip.bat
@@ -0,0 +1,3 @@
+pushd DependencyChecker\bin\Debug
+"c:\Program Files\7-Zip\7z.exe" a ..\..\..\DotNetArchitectureChecker.zip DependencyChecker.exe Mono.Cecil.dll Mono.Cecil.Mdb.dll Mono.Cecil.Pdb.dll
+popd
