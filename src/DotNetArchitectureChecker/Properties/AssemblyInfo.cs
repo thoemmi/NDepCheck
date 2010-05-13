3be6197fb5dd1987f9c53fe8d9991f57502b17b7
hmmueller <harald_m_mueller@gmx.de>
2010-05-13 21:20:19 +0200

    [svn r36] Version corrected to 1.6

--- src/DotNetArchitectureChecker/Properties/AssemblyInfo.cs
+++ src/DotNetArchitectureChecker/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using DotNetArchitectureChecker;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DotNetArchitectureChecker 1.5")]
+[assembly: AssemblyTitle("DotNetArchitectureChecker 1.6")]
 [assembly: AssemblyDescription("Architecture Checker for .Net")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -30,5 +30,5 @@ using DotNetArchitectureChecker;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
