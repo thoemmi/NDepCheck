554708969eb4e03ea21d415fea56c92d1a689c26
Thomas Freudenberg <info@thomasfreudenberg.com>
2012-10-10 10:32:48 +0200

    case-insensitive filenames

--- src/NDepCheck/DirectoryOption.cs
+++ src/NDepCheck/DirectoryOption.cs
@@ -1,14 +1,17 @@
-﻿using System.IO;
+﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace NDepCheck {
     public class DirectoryOption {
-        private readonly IDictionary<string, string> _smallToFullMap = new Dictionary<string, string>();
+        private readonly IDictionary<string, string> _smallToFullMap = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+
         public DirectoryOption(string path, bool recurse) {
             var o = recurse ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
             Add(path, "*.dll.dep", o);
             Add(path, "*.exe.dep", o);
         }
+
         private void Add(string path, string pattern, SearchOption o) {
             foreach (var f in Directory.GetFiles(path, pattern, o)) {
                 var fileName = Path.GetFileName(f);
@@ -20,6 +23,7 @@ namespace NDepCheck {
                 }
             }
         }
+
         public string GetFullNameFor(string filename) {
             string result;
             _smallToFullMap.TryGetValue(filename, out result);
