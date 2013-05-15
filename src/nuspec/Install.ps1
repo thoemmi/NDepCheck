6c179aab31e4a0795dbaec75319c4405507515c8
Thomas Freudenberg <info@thomasfreudenberg.com>
2013-05-15 20:03:23 +0200

    created nuspec and supporting files

---
+++ src/nuspec/Install.ps1
@@ -0,0 +1,13 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$RootNamespace = $project.Properties.Item("RootNamespace").Value
+$FullPath = $project.Properties.Item("FullPath").Value
+$OutputFileName = $project.Properties.Item("OutputFileName").Value
+
+$depfilename = "$FullPath$OutputFileName.dep"
+
+"Creating dummy dependency rule file and adding it to the project"
+
+Set-Content -Path $depfilename -Value "$RootNamespace.* ---> System.*"
+
+$project.ProjectItems.AddFromFile($depfilename)
