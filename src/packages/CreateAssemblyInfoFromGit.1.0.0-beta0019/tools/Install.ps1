a8ef793ddbdc0d6dd72f033d3f39887e69a71cba
Thomas Freudenberg <info@thomasfreudenberg.com>
2013-06-01 16:09:00 +0200

    must commit packages to git, otherwise projects won't find CreateAssemblyInfoFromGit

---
+++ src/packages/CreateAssemblyInfoFromGit.1.0.0-beta0019/tools/Install.ps1
@@ -0,0 +1,5 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$assemblyinfofile = Join-Path (Split-Path $project.DTE.Solution.FullName) "CommonAssemblyInfo.cs"
+
+$project.ProjectItems.AddFromFile($assemblyinfofile)
