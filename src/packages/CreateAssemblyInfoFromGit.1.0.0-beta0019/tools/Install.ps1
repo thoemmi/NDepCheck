64af318fb22ea0b2c5729faf49db367177c8fb70
Thomas Freudenberg <info@thomasfreudenberg.com>
2014-01-07 21:39:32 +0100

    bumbed version to 2.1

--- src/packages/CreateAssemblyInfoFromGit.1.0.0-beta0019/tools/Install.ps1
+++
@@ -1,5 +0,0 @@
-﻿param($installPath, $toolsPath, $package, $project)
-
-$assemblyinfofile = Join-Path (Split-Path $project.DTE.Solution.FullName) "CommonAssemblyInfo.cs"
-
-$project.ProjectItems.AddFromFile($assemblyinfofile)
